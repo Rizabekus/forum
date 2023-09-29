129a44561198955ff2dbbc2cf0f2b027d4eaa362
Rizabekus <rizabek.zhampeisov@gmail.com>
2023-09-29 16:16:41 +0600

    minor changes

--- internal/models/models.go
+++ internal/models/models.go
@@ -19,3 +19,21 @@ type (
 	CommentService       interface{}
 	LikesDislikesService interface{}
 )
+
+type Comment struct {
+	Name     string
+	Text     string
+	Comid    int
+	Likes    int
+	Dislikes int
+}
+
+type Post struct {
+	Title    string
+	Text     string
+	Name     string
+	Category string
+	Id       int
+	Likes    int
+	Dislikes int
+}
