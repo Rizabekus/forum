a6ccb05d118d1902264c3468f2c17cc7758dfefa
Rizabekus <rizabek.zhampeisov@gmail.com>
2023-10-15 19:49:06 +0600

    minor changes

--- internal/controllers/postconfirmation.go
+++ internal/controllers/postconfirmation.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"forum/pkg"
+	"io/ioutil"
 	"net/http"
 	"text/template"
 )
@@ -12,12 +13,28 @@ func (controllers *Controllers) PostConfirmation(w http.ResponseWriter, r *http.
 		controllers.ErrorHandler(w, http.StatusMethodNotAllowed)
 		return
 	}
-	// cookie, err := r.Cookie("logged-in")
+	if err := r.ParseMultipartForm(20 * 1024 * 1024); err != nil {
+		// redirect to the same page and add text asking for file less than 20mb
+		fmt.Println("qweqweqw")
+		controllers.ErrorHandler(w, http.StatusInternalServerError)
+		return
+	}
+	cookie, err := r.Cookie("logged-in")
 
 	title := r.FormValue("title")
 	text := r.FormValue("convert")
-	// cat := r.FormValue("cars")
+	cat := r.FormValue("cars")
 	image, _, err := r.FormFile("image")
+	if err != nil {
+		fmt.Println("qweqweqw")
+		controllers.ErrorHandler(w, http.StatusInternalServerError)
+		return
+	}
+
+	defer image.Close()
+
+	// Read the file content into a []byte
+	imageData, err := ioutil.ReadAll(image)
 	if err != nil {
 		controllers.ErrorHandler(w, http.StatusInternalServerError)
 		return
@@ -32,7 +49,7 @@ func (controllers *Controllers) PostConfirmation(w http.ResponseWriter, r *http.
 		}
 		tmpl.Execute(w, t)
 	} else {
-		// controllers.Service.PostService.CreatePost(cookie.Value, text, cat, title, image)
+		controllers.Service.PostService.CreatePost(cookie.Value, text, cat, title, imageData)
 		http.Redirect(w, r, "/", 302)
 	}
 }
